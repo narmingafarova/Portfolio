c2a3128d52e9ad0bcddd711c78be8d02ab7be60a
Gafarli Tutu <qeferlitutu@gmail.com>
2022-07-14 20:48:07 +0400

    profession initial page

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(function () {
+//     $(document).scroll(function () {
+//       var $nav = $(".navbar-fixed-top");
+//       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+//   });
+
+
+// .navbar-fixed-top.scrolled {
+//     background-color: #fff !important;
+//     transition: background-color 200ms linear;
+//   }
